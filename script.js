9c211957c529188fe0c2928e16ad4e20d566b684
shinlee <shinlee@gmail.com>
2020-10-28 14:04:11 -0700

    include getNetworkStats info into DB

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ async function createRoom() {
 	updateRoomInfoDisplay();
 	setInterval(getRoomInfo, 5000);
 	setInterval(updateNetworkInfoDisplay, 15000);
-	setInterval(saveRoomDataintoDB, 5000);
+	setInterval(saveRoomDataintoDB, 15000);
 }
 
 async function createFrame() {
@@ -125,10 +125,17 @@ async function saveRoomDataintoDB() {
 
 	if (localStorage) {
 		var roomId;
+		var videoData;
 		if (!localStorage['roomId']) {
 			roomId = [];
 		} else {
 			roomId = JSON.parse(localStorage['roomId']);
+		}
+
+		if (!localStorage['videoData']) {
+			videoData = [];
+		} else {
+			videoData = JSON.parse(localStorage['videoData']);
 		}
 
 		if (!(roomId instanceof Array)) roomId = [];
@@ -137,14 +144,32 @@ async function saveRoomDataintoDB() {
 			localStorage.setItem('roomId', JSON.stringify(roomId));
 		}
 
+		if (roomInfo.id != null) {
+			json.roomId = roomInfo.id;
+			json.timestamp = statsInfo.stats.latest.timestamp;
+			json.videoRecvBitsPerSecond = Math.floor(statsInfo.stats.latest.videoRecvBitsPerSecond / 1000);
+			json.videoSendBitsPerSecond = Math.floor(statsInfo.stats.latest.videoSendBitsPerSecond / 1000);
+			json.videoRecvPacketLoss = Math.floor(statsInfo.stats.videoRecvPacketLoss * 100)
+			json.videoSendPacketLoss = Math.floor(statsInfo.stats.videoSendPacketLoss * 100)
+			videoData.push(json)
+			localStorage.setItem('videoData', JSON.stringify(videoData));
+		}
+		console.log(localStorage);
+
 		// Retrieve
 		let storageEl = document.getElementById("result");
 		storageEl.innerHTML = `
 		<div><h1> Dashboard </h1></div>
 		${localStorage.getItem("roomId")}
 		`
+
+		let DatastorageEl = document.getElementById("videoData");
+		DatastorageEl.innerHTML = `
+			<div><h1> Video Data </h1></div>
+				${localStorage.getItem("videoData")}
+		`
 	} else {
 		let storageEl = document.getElementById("result");
 		storageEl.innerHTML = `Sorry, your browser does not support Web Storage...`
-	}
+	};
 }
